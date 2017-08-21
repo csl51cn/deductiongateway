c3b77f03468462ede56d5f97b0455f0639bb8382
sili chen <178905831@qq.com>
2017-08-21 14:31:42 +0800

    更新错误反馈文字描述

--- src/main/java/org/starlightfinancial/deductiongateway/utility/Utility.java
+++ src/main/java/org/starlightfinancial/deductiongateway/utility/Utility.java
@@ -9,15 +9,15 @@ import java.util.Map;
 
 public class Utility {
 
-    public static final String SEND_BANK_URL = "https://payment.chinapay.com/CTITS/cpeduinterface/OrderGet.do";//生产地址
-    //    public static final String SEND_BANK_URL = "http://newpayment-test.chinapay.com/CTITS/cpeduinterface/OrderGet.do";//测试环境
+    //    public static final String SEND_BANK_URL = "https://payment.chinapay.com/CTITS/cpeduinterface/OrderGet.do";//生产地址
+    public static final String SEND_BANK_URL = "http://newpayment-test.chinapay.com/CTITS/cpeduinterface/OrderGet.do";//测试环境
     public static final String SEND_BANK_BGRETURL = "http://113.204.117.46:8081/PayGetBgAsyn";//后台交易接收URL地址
     public static final String SEND_BANK_PAGERETURL = "http://113.204.117.46:8081/PayGetPgAsyn";//页面交易接收URL地址
-    public static final String SEND_BANK_KEY_FILE = "MerPrK_808080201302851_20131030113446.key";//生产环境私钥文件名
-    //    public static final String SEND_BANK_KEY_FILE = "MerPrK808080290000001.key";//测试环境私钥文件名
+    //    public static final String SEND_BANK_KEY_FILE = "MerPrK_808080201302851_20131030113446.key";//生产环境私钥文件名
+    public static final String SEND_BANK_KEY_FILE = "MerPrK808080290000001.key";//测试环境私钥文件名
     public static final String SEND_BANK_KEY_PUB_FILE = "PgPubk.key";//公钥文件名
-    public static final String SEND_BANK_MERID = "808080201302851";//生产环境
-    //    public static final String SEND_BANK_MERID = "808080290000001";//测试环境
+    //    public static final String SEND_BANK_MERID = "808080201302851";//生产环境
+    public static final String SEND_BANK_MERID = "808080290000001";//测试环境
     public static final String SEND_BANK_VERSION = "20100401";//版本号curyId
     public static final String SEND_BANK_GATEID = "7008";//网关
     public static final String SEND_BANK_CURYID = "156";//交易币种
