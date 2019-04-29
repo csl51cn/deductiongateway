a98214372ad9c73aee9eaaf2e059c6e3c4d5d3ef
sili.chen <csl51cn@hotmail.com>
2019-04-29 14:49:46 +0800

    更新系统自动代扣分账

--- src/main/java/org/starlightfinancial/deductiongateway/service/impl/Splitter.java
+++ src/main/java/org/starlightfinancial/deductiongateway/service/impl/Splitter.java
@@ -55,28 +55,8 @@ public class Splitter extends Decorator {
         BigDecimal drawDownAmount = totalAmount;
         BigDecimal drawDownBxAmount = autoBatchDeduction.getBxAmount();
         BigDecimal drawDownFwfAmount = autoBatchDeduction.getFwfAmount();
-
-        while (drawDownAmount.doubleValue() > 0) {
-            BigDecimal bxAmount;
-            if (drawDownAmount.doubleValue() > singleLimit.doubleValue()) {
-                bxAmount = singleLimit.subtract(drawDownFwfAmount);
-                if (bxAmount.doubleValue() >= singleLimit.doubleValue()) {
-                    bxAmount = singleLimit;
-                }
-            } else {
-                bxAmount = drawDownAmount;
-            }
-            AutoBatchDeduction newObj = new AutoBatchDeduction();
-            BeanUtils.copyProperties(autoBatchDeduction, newObj);
-            newObj.setBxAmount(bxAmount);
-            newObj.setFwfAmount(drawDownFwfAmount);
-            drawDownAmount = drawDownAmount.subtract(bxAmount).subtract(drawDownFwfAmount);
-            drawDownBxAmount = drawDownBxAmount.subtract(bxAmount);
-            drawDownFwfAmount = drawDownFwfAmount.subtract(drawDownFwfAmount);
-
-            result.add(newObj);
-        }
-
+        drawDownBxAmount = dealFwf(drawDownBxAmount, drawDownFwfAmount, singleLimit, result);
+        dealBx(drawDownBxAmount, singleLimit, result);
         return result;
     }
 
@@ -92,5 +72,64 @@ public class Splitter extends Decorator {
 
     public void setAutoBatchDeduction(AutoBatchDeduction autoBatchDeduction) {
         this.autoBatchDeduction = autoBatchDeduction;
+    }
+
+    /**
+     * @param drawDownBxAmount  1
+     * @param drawDownFwfAmount 2
+     * @param singleLimit       3
+     * @param list              4
+     * @return java.math.BigDecimal
+     * @Author sili.chen
+     * @Description 处理服务费
+     * @Date 14:48 2019/4/29
+     **/
+    private BigDecimal dealFwf(BigDecimal drawDownBxAmount, BigDecimal drawDownFwfAmount, BigDecimal singleLimit, List list) {
+        BigDecimal bxAmount;
+        BigDecimal fwfAmount;
+        while (drawDownFwfAmount.doubleValue() > 0) {
+            if (drawDownFwfAmount.doubleValue() >= singleLimit.doubleValue()) {
+                bxAmount = new BigDecimal(0);
+                fwfAmount = singleLimit;
+            } else {
+                bxAmount = singleLimit.subtract(drawDownFwfAmount);
+                fwfAmount = drawDownFwfAmount;
+            }
+            AutoBatchDeduction newObj = new AutoBatchDeduction();
+            BeanUtils.copyProperties(autoBatchDeduction, newObj);
+            newObj.setBxAmount(bxAmount);
+            newObj.setFwfAmount(fwfAmount);
+            list.add(newObj);
+            drawDownBxAmount = drawDownBxAmount.subtract(bxAmount);
+            drawDownFwfAmount = drawDownFwfAmount.subtract(fwfAmount);
+        }
+        return drawDownBxAmount;
+    }
+
+    /**
+     * @param drawDownBxAmount 1
+     * @param singleLimit      2
+     * @param result           3
+     * @return void
+     * @Author sili.chen
+     * @Description 处理本息
+     * @Date 14:48 2019/4/29
+     **/
+    private void dealBx(BigDecimal drawDownBxAmount, BigDecimal singleLimit, List result) {
+        BigDecimal bxAmount;
+        BigDecimal fwfAmount = new BigDecimal(0);
+        while (drawDownBxAmount.doubleValue() > 0) {
+            if (drawDownBxAmount.doubleValue() >= singleLimit.doubleValue()) {
+                bxAmount = singleLimit;
+            } else {
+                bxAmount = drawDownBxAmount;
+            }
+            AutoBatchDeduction newObj = new AutoBatchDeduction();
+            BeanUtils.copyProperties(autoBatchDeduction, newObj);
+            newObj.setBxAmount(bxAmount);
+            newObj.setFwfAmount(fwfAmount);
+            result.add(newObj);
+            drawDownBxAmount = drawDownBxAmount.subtract(bxAmount);
+        }
     }
 }
